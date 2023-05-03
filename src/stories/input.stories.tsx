84d72af5d3ddeaa7c57581acdf8da6b583032e75
Stanislav <stanislavasilevich@gmail.com>
2023-05-03 13:56:46 +0300

    создал input.stories.tsx

---
+++ src/stories/input.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default {
+  title: 'Example/input',
+  // component: Input,
+};
+
+export const UncontrolledInput = () => <input />;
+export const ControlledInput = () => <input value={'hello'} />;
